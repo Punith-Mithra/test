168066969d293089039a67acf137ddfcae144a5b
Punith-Mithra <u.punithmithra@gmail.com>
2022-06-19 18:29:53 +0400

    1 file modified, 1 deleted

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import _ from 'lodash';
 import style from './styles.css';
 
 function component() {
-  const element = document.createElement('div');
+  const element = document.createElement('img');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.src = './images/1.jpg';
+  element.style = { width: '100px' };
 
   return element;
 }
